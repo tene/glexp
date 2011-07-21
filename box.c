4857dbfd507b15ac8e41ae5de4b4eae5d622e789
Stephen Weeks <tene@allalone.org>
2011-07-21 04:12:05 -0700

    Clear the depth buffer on render

--- box.c
+++ box.c
@@ -65,7 +65,8 @@ float aspect_ratio = 1;
 
 void render() {
     glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
-    glClear( GL_COLOR_BUFFER_BIT );
+    glClearDepth(1.0f);
+    glClear( GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT );
 
     glUseProgram(glprog);
 
